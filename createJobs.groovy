c1e63c5e5ed4a7b5ee8fd1bc82c242a4ab19b3a9
Georgyi1982Fox <greserve12@yahoo.com>
2023-04-13 09:52:22 +0300

    "seedJob corrected"

--- createJobs.groovy
+++ createJobs.groovy
@@ -7,3 +7,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('taskService_new') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/Georgyi1982Fox/taskservice_new.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
